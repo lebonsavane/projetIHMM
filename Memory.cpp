59a356a91abb6d22988f4a05a5757c821c140247
lebonsavane <39559339+lebonsavane@users.noreply.github.com>
2018-06-07 08:53:22 +0200

    Create Memory.cpp

---
+++ Memory.cpp
@@ -0,0 +1,38 @@
+#include "Memory.h"
+
+BEGIN_EVENT_TABLE(Principale, wxFrame)
+    EVT_CLOSE(Principale::onClose)
+    EVT_BUTTON (wxID_ANY, MemoryPrincipale::demarrerPartie)
+    EVT_BUTTON (wxID_ANY, MemoryPrincipale::abandonnerPartie)
+    EVT_TIMER(TIMER_ID, MemoryPrincipale::OnTimer)
+
+
+END_EVENT_TABLE()
+
+PrincipaleMemory::PrincipaleMemory()
+                 :m_timer(this, TIMER_ID)
+{
+    m_timer.Start(1000);
+}
+void PrincipaleMemory::terminerPartieTempsRestantEcoule(wxTimerEvent& event)
+{
+    if (m_timer=0)
+    {
+     int temps0 = wxMessageBox( wxT ("Temps écoulé, fin du jeu"), wxT("PERDU :(("), wxOK, this);
+     
+
+
+
+    }
+
+void PrincipaleMemory::terminerPartieCarteRassembler()
+{
+
+}
+}
+
+}
+Memory::~Memory()
+{
+    //dtor
+}
