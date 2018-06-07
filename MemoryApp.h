f9843cb29e29591f65f9e5740557a0bce794985e
lebonsavane <39559339+lebonsavane@users.noreply.github.com>
2018-06-07 08:56:34 +0200

    Update MemoryApp.h

--- MemoryApp.h
+++ MemoryApp.h
@@ -1,1 +1,21 @@
 
+#DEFINE MEMORYAPP_H
+#include "MemoryApp.h"
+#include "Memory.h"
+
+// Permet à wxWidgets de crééer l'instance d'application
+IMPLEMENT_APP(MemoryApp)
+
+// Implémentation de la méthode OnInit()
+   bool MemoryApp::OnInit()
+   {
+       // Création de la fenêtre principale de l'application
+       Principale *maFen = new Principale(wxT("Jeu du Memory"));
+       // Rendre la fenêtre visible
+       maFen -> Show(true);
+
+       // Démarre la boucle des messages
+       return true;
+    }
+
+
